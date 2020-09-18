30b36bf1f39b591fa0406c50461f185024e0ea24
ali raza <aliahmadraza401@gmail.com>
2020-09-18 11:13:33 +0500

    Updates

---
+++ precache-manifest.15e182d675e1dabacc06a20a24472548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f0b29cdded763a23f4c0557ae6f714",
+    "url": "/Digital-Clock/index.html"
+  },
+  {
+    "revision": "158fe945a8180c5ddc38",
+    "url": "/Digital-Clock/static/css/main.78322179.chunk.css"
+  },
+  {
+    "revision": "3560bbb995b220b51030",
+    "url": "/Digital-Clock/static/js/2.2e009a0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Digital-Clock/static/js/2.2e009a0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "158fe945a8180c5ddc38",
+    "url": "/Digital-Clock/static/js/main.7c234e47.chunk.js"
+  },
+  {
+    "revision": "93d15c4bf11831eee9c8",
+    "url": "/Digital-Clock/static/js/runtime-main.6999f17c.js"
+  }
+]);
